7262a52539764a12919a4ccbd3bc6a3fb90e86f3
Albert Brazo <albertbs45@gmail.com>
2022-01-24 00:51:35 +0100

    implementing creating or deleting a user and authorities when a manufacturer is created or deleted

--- src/main/java/com/catalog/beercatalog/service/ManufacturerService.java
+++ src/main/java/com/catalog/beercatalog/service/ManufacturerService.java
@@ -1,15 +1,23 @@
 package com.catalog.beercatalog.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.catalog.beercatalog.entity.Authority;
+import com.catalog.beercatalog.entity.Beer;
 import com.catalog.beercatalog.entity.Manufacturer;
+import com.catalog.beercatalog.entity.Provider;
+import com.catalog.beercatalog.repository.AuthorityRepository;
+import com.catalog.beercatalog.repository.BeerRepository;
 import com.catalog.beercatalog.repository.ManufacturerRepository;
+import com.catalog.beercatalog.repository.ProviderRepository;
 import com.catalog.beercatalog.utils.ServiceUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 @Service
@@ -18,6 +26,16 @@ public class ManufacturerService {
     @Autowired
     private ManufacturerRepository manufacturerRepo;
 
+    @Autowired
+    private BeerRepository beerRepo;
+
+    @Autowired
+    private ProviderRepository providerRepo;
+
+    @Autowired
+    private AuthorityRepository authorityRepo;
+
+    @Transactional(readOnly = true)
     public Page<Manufacturer> findAll(
         String name,
         String nationality,
@@ -44,6 +62,7 @@ public class ManufacturerService {
         return pagedResult; 
     }
 
+    @Transactional(readOnly = true)
     public Manufacturer findById(Long id) {
 
         Optional<Manufacturer> manufacturerOpt = manufacturerRepo.findById(id);
@@ -57,17 +76,42 @@ public class ManufacturerService {
         return manufacturer;
     }
 
+    @Transactional
     public Manufacturer save(Manufacturer manufacturer) {
+
         Manufacturer manufacturerAddedOrUpdated = manufacturerRepo.save(manufacturer);
 
-        if (manufacturerAddedOrUpdated == null) {
-            // TODO exception handling
+        // It was an insertion
+        if (manufacturer.getId() == 0) {
+
+            //Insert user/provider
+            String normalizedUsername = manufacturer.getName().replaceAll("\\s+","").trim().toLowerCase().concat("@provider.com");
+
+            Provider provider = new Provider(
+                normalizedUsername, "$2a$12$tu/q0PZskDF9kk.KRPnys.aQcnqU6JdLPOp4bOjIgfwK07N4nSDRC", manufacturerAddedOrUpdated, null);
+
+            Provider insertedProvider = providerRepo.save(provider);
+
+            // Insert related authority to new created provider
+            Authority authority = new Authority("ROLE_MANUFACTURER", insertedProvider);
+            authorityRepo.save(authority);
         }
 
         return manufacturerAddedOrUpdated;
     }
 
+    @Transactional
     public void deleteById(Long id) {
+
+        //Deleting all beers related to the manufacturer
+        List<Beer> beersRelatedToManufacturer = beerRepo.findAllBeersWithPaginationAndSorting(
+            null, null, null, null, id, null, null, null).getContent();
+        beerRepo.deleteAll(beersRelatedToManufacturer);
+
+        //Deleting user/provider and authorities related to the manufacturer
+        providerRepo.deleteByManufacturer_Id(id);
+
+        //Deleting manufacturer
         manufacturerRepo.deleteById(id);
     }
     
