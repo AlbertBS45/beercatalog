9fe900ab2c6cdf370fa3c7ab057c3812bba9a5f3
Albert Brazo <albertbs45@gmail.com>
2022-01-27 17:40:11 +0100

    changing annotation location for custom method expression managed through a bean now

--- src/main/java/com/catalog/beercatalog/config/SecurityConfig.java
+++ src/main/java/com/catalog/beercatalog/config/SecurityConfig.java
@@ -3,14 +3,16 @@ package com.catalog.beercatalog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
     private static final String[] AUTH_WHITELIST = {"/beers", "/beers/*", "/manufacturers", "manufacturers/*"};
 
     @Override
